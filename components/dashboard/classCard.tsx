45f5c342556f32d84feff652c13fbe2448adbf48
TinsellyCone <13maxwellwise@gmail.com>
2023-02-23 10:12:06 -0500

    Dashboard now shows assignments for each class

--- components/dashboard/classCard.tsx
+++ components/dashboard/classCard.tsx
@@ -15,6 +15,8 @@ import {
 import { IconNotebook, IconDotsVertical, IconCheck } from '@tabler/icons'
 import Link from 'next/link'
 import { useState } from 'react'
+import useSWR from 'swr'
+import useToken from 'components/lib/useToken'
 
 function ActiveBadge(props) {
   if (props.active) {
@@ -25,53 +27,69 @@ function ActiveBadge(props) {
 
 export default function ClassCard(props) {
   // imagePath, className, grade, active
+  const { token } = useToken()
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "/canvas/courses/" +
+      props.id +
+      "/assignments?access_token=" +
+      token +
+      "&per_page=2&order_by=due_at",
+    fetcher
+  );
+  console.log(data)
 
   const [opened, setOpened] = useState(false)
   return (
-    <Card w={300} shadow={'lg'} radius={'md'} p={'lg'} withBorder>
+    <Card w={300} shadow={"lg"} radius={"md"} p={"lg"} withBorder>
       <Card.Section>
         <Image src={props.imagePath} height={175} withPlaceholder></Image>
       </Card.Section>
-      <Group position='apart' mt={'md'} mb={'xs'}>
-        <Stack justify={'center'} spacing={0}>
-          <Text size={'lg'} weight={600} m={0} lineClamp={1} maw={170}>
+      <Group position="apart" mt={"md"} mb={"xs"}>
+        <Stack justify={"center"} spacing={0}>
+          <Text size={"lg"} weight={600} m={0} lineClamp={1} maw={170}>
             {props.className}
           </Text>
-          <Text m={0} size={'xs'} color={'dimmed'} maw={120}>
-            {props.grade + '% Average'}
+          <Text m={0} size={"xs"} color={"dimmed"} maw={120}>
+            {props.grade + "% Average"}
           </Text>
         </Stack>
         <ActiveBadge active={props.active} />
       </Group>
 
-      <Divider mb={'sm'}></Divider>
+      <Divider mb={"sm"}></Divider>
 
-      <Stack spacing={'xs'}>
-        <Group position='left' spacing={'xs'}>
-          <IconNotebook size={'20'} color={'gray'} />
-          <Text color={'dimmed'} size={'sm'} lineClamp={1} maw={200}>
-            Sprial Homework
+      <Stack spacing={"xs"}>
+        <Group position="left" spacing={"xs"}>
+          <IconNotebook size={"20"} color={"gray"} />
+          <Text color={"dimmed"} size={"sm"} lineClamp={1} maw={200}>
+            {data && data[0] != null ? data[0].name : <></>}
           </Text>
         </Group>
-        <Group position='left' spacing={'xs'}>
-          <IconNotebook size={'20'} color={'gray'} />
-          <Text color={'dimmed'} size={'sm'} lineClamp={1} maw={200}>
-            Review Guide
+        <Group position="left" spacing={"xs"}>
+          <IconNotebook size={"20"} color={"gray"} />
+          <Text color={"dimmed"} size={"sm"} lineClamp={1} maw={200}>
+            {data && data[1] != null ? data[1].name : <></>}
           </Text>
         </Group>
       </Stack>
-      <Group position='center' mt={'md'} spacing={'xs'}>
+      <Group position="center" mt={"md"} spacing={"xs"}>
         <Button
-          variant='light'
+          variant="light"
           component={Link}
-          href={'/class/' + props.id}
+          href={"/class/" + props.id}
           color={props.color}
           style={{ flex: 1 }}
           radius={process.env.NEXT_PUBLIC_RADIUS}
         >
           View
         </Button>
-        <ActionIcon size={36} variant='light' onClick={() => setOpened(true)} radius={process.env.NEXT_PUBLIC_RADIUS}>
+        <ActionIcon
+          size={36}
+          variant="light"
+          onClick={() => setOpened(true)}
+          radius={process.env.NEXT_PUBLIC_RADIUS}
+        >
           <IconDotsVertical size={20} />
         </ActionIcon>
       </Group>
@@ -79,36 +97,36 @@ export default function ClassCard(props) {
       <Modal
         opened={opened}
         onClose={() => setOpened(false)}
-        title='Edit Card'
+        title="Edit Card"
         centered
       >
-        <Badge color={'red'}>Placeholder</Badge>
+        <Badge color={"red"}>Placeholder</Badge>
         <ColorInput
-          placeholder='#ffffff'
-          label='Card Color'
+          placeholder="#ffffff"
+          label="Card Color"
           value={props.cardColor}
           my={10}
           radius={process.env.NEXT_PUBLIC_INPUT_RADIUS}
         />
-        <Badge color={'red'}>Placeholder</Badge>
+        <Badge color={"red"}>Placeholder</Badge>
         <NumberInput
           defaultValue={props.cardPosition}
           placeholder={props.cardPosition}
-          label='Card Position'
+          label="Card Position"
           my={10}
           min={1}
           radius={process.env.NEXT_PUBLIC_INPUT_RADIUS}
         />
         <Button
           mt={10}
-          variant={'light'}
+          variant={"light"}
           onClick={() => setOpened(false)}
-          color={'gray'}
+          color={"gray"}
           radius={process.env.NEXT_PUBLIC_RADIUS}
         >
           Close
         </Button>
       </Modal>
     </Card>
-  )
+  );
 }
